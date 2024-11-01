c1176dcb0f152be9bd49f862cf23ad033ca5c649
AdamYuan <y13916619121@126.com>
2024-11-01 15:04:14 +0800

    Improve the interface for Sub-Splat

--- Assets/UnityGaussianSplatting/Shaders/SubSplat.hlsl
+++ Assets/UnityGaussianSplatting/Shaders/SubSplat.hlsl
@@ -22,32 +22,38 @@
     #error MAX_SUB_SPLAT_LEVEL too large
 #endif
 
+#define __PUBLIC__
+// Interface are marked with __PUBLIC__
+
 // Remember to change kGpuSubSplatDataSize in GaussianSplatRenderer.cs if you modify `struct SubSplatData`
-struct SubSplatData {
+__PUBLIC__ struct SubSplatData {
     float3 pos;
     uint rootSplatID; // Inherit the SH from Root-Splat
     uint2 rotFP16x4;
     uint2 scaleOpacityFP16x4;
 };
 
-SubSplatData GetSubSplatFromSplat(uint splatID, in const SplatData splat) {
-    SubSplatData ret;
+// A unpacked version of struct SubSplatData, not necessary
+struct UnpackedSubSplatData {
+    float3 pos;
+    uint rootSplatID; // Inherit the SH from Root-Splat
+    float4 rot;
+    float3 scale;
+    float opacity;
+};
+
+UnpackedSubSplatData GetUnpackedSubSplatFromSplat(uint splatID, in const SplatData splat) {
+    UnpackedSubSplatData ret;
     ret.pos = splat.pos;
+    ret.rot = splat.rot;
+    ret.scale = splat.scale;
+    ret.opacity = splat.opacity;
     ret.rootSplatID = splatID;
-    ret.rotFP16x4 = uint2(
-        f32tof16(splat.rot.x) | f32tof16(splat.rot.y) << 16, 
-        f32tof16(splat.rot.z) | f32tof16(splat.rot.w) << 16
-    );
-    ret.scaleOpacityFP16x4 = uint2(
-        f32tof16(splat.scale.x) | f32tof16(splat.scale.y) << 16, 
-        f32tof16(splat.scale.z) | f32tof16(float(splat.opacity)) << 16
-    );
     return ret;
 }
 
-SplatData GetSplatFromSubSplat(in const SubSplatData subSplat) {
-    SplatData ret;
-    ret = LoadSplatData(subSplat.rootSplatID);
+UnpackedSubSplatData UnpackSubSplatData(in const SubSplatData subSplat) {
+    UnpackedSubSplatData ret;
     ret.pos = subSplat.pos;
     ret.rot = float4(
         f16tof32(subSplat.rotFP16x4.x), f16tof32(subSplat.rotFP16x4.x >> 16),
@@ -58,22 +64,88 @@ SplatData GetSplatFromSubSplat(in const SubSplatData subSplat) {
         f16tof32(subSplat.scaleOpacityFP16x4.y)
     );
     ret.opacity = f16tof32(subSplat.scaleOpacityFP16x4.y >> 16);
+    ret.rootSplatID = subSplat.rootSplatID;
     return ret;
 }
 
-void SplitSubSplat(in const SubSplatData parentSubSplat, out SubSplatData o_subSplat0, out SubSplatData o_subSplat1) {
+SubSplatData PackSubSplatData(in const UnpackedSubSplatData subSplat) {
+    SubSplatData ret;
+    ret.pos = subSplat.pos;
+    ret.rotFP16x4 = uint2(
+        f32tof16(subSplat.rot.x) | f32tof16(subSplat.rot.y) << 16, 
+        f32tof16(subSplat.rot.z) | f32tof16(subSplat.rot.w) << 16
+    );
+    ret.scaleOpacityFP16x4 = uint2(
+        f32tof16(subSplat.scale.x) | f32tof16(subSplat.scale.y) << 16, 
+        f32tof16(subSplat.scale.z) | f32tof16(subSplat.opacity) << 16
+    );
+    ret.rootSplatID = subSplat.rootSplatID;
+    return ret;
+}
+
+void SplitUnpackedSubSplat(
+    in const UnpackedSubSplatData parentSubSplat, 
+    out UnpackedSubSplatData o_subSplat0, 
+    out UnpackedSubSplatData o_subSplat1
+) {
     // TODO: Implement your splitting behavior
-    o_subSplat0 = parentSubSplat;
-    o_subSplat1 = parentSubSplat;
+    UnpackedSubSplatData subSplat0 = parentSubSplat, subSplat1 = parentSubSplat;
+    float3 scale = parentSubSplat.scale * 0.5;
+    subSplat0.scale = scale;
+    subSplat0.pos += scale;
+    subSplat1.scale = scale;
+    subSplat1.pos -= scale;
+
+    o_subSplat0 = subSplat0;
+    o_subSplat1 = subSplat1;
+}
+
+void MergeUnpackedSubSplat(
+    in const UnpackedSubSplatData subSplat0, 
+    in const UnpackedSubSplatData subSplat1, 
+    inout UnpackedSubSplatData io_parentSubSplat
+) {
+    // TODO: Implement this if needed, and remember to set SUB_SPLAT_MERGE to 1
+}
+
+__PUBLIC__ SplatData GetSplatFromSubSplat(in const SubSplatData subSplat) {
+    UnpackedSubSplatData unpackedSubSplat = UnpackSubSplatData(subSplat);
+    SplatData ret;
+    ret = LoadSplatData(subSplat.rootSplatID); // Load SH
+    ret.pos = unpackedSubSplat.pos;
+    ret.rot = unpackedSubSplat.rot;
+    ret.scale = unpackedSubSplat.scale;
+    ret.opacity = unpackedSubSplat.opacity;
+    return ret;
+}
+
+__PUBLIC__ void InitSubSplat(uint rootSplatID, in const SplatData rootSplat, out SubSplatData o_subSplat0, out SubSplatData o_subSplat1) {
+    UnpackedSubSplatData unpackedRootSubSplat = GetUnpackedSubSplatFromSplat(rootSplatID, rootSplat);
+    UnpackedSubSplatData unpackedSubSplat0, unpackedSubSplat1;
+    SplitUnpackedSubSplat(unpackedRootSubSplat, unpackedSubSplat0, unpackedSubSplat1);
+    o_subSplat0 = PackSubSplatData(unpackedSubSplat0);
+    o_subSplat1 = PackSubSplatData(unpackedSubSplat1);
+}
+
+__PUBLIC__ void SplitSubSplat(in const SubSplatData parentSubSplat, out SubSplatData o_subSplat0, out SubSplatData o_subSplat1) {
+    UnpackedSubSplatData unpackedParentSubSplat = UnpackSubSplatData(parentSubSplat);
+    UnpackedSubSplatData unpackedSubSplat0, unpackedSubSplat1;
+    SplitUnpackedSubSplat(unpackedParentSubSplat, unpackedSubSplat0, unpackedSubSplat1);
+    o_subSplat0 = PackSubSplatData(unpackedSubSplat0);
+    o_subSplat1 = PackSubSplatData(unpackedSubSplat1);
 }
 
 #if SUB_SPLAT_MERGE == 1
-    void MergeSubSplat(in const SubSplatData subSplat0, in const SubSplatData subSplat1, inout SubSplatData io_parentSubSplat) {
-        // TODO: Implement this if needed, and remember to set SUB_SPLAT_MERGE to 1
+    __PUBLIC__ void MergeSubSplat(in const SubSplatData subSplat0, in const SubSplatData subSplat1, inout SubSplatData io_parentSubSplat) {
+        UnpackedSubSplatData unpackedSubSplat0 = UnpackSubSplatData(subSplat0);
+        UnpackedSubSplatData unpackedSubSplat1 = UnpackSubSplatData(subSplat1);
+        UnpackedSubSplatData unpackedParentSubSplat = UnpackSubSplatData(io_parentSubSplat);
+        MergeUnpackedSubSplat(unpackedSubSplat0, unpackedSubSplat1, unpackedParentSubSplat);
+        io_parentSubSplat = PackSubSplatData(unpackedParentSubSplat);
     }
 #endif
 
-uint CalcSplatLevel(
+__PUBLIC__ uint CalcSplatLevel(
     uint splatID, in const SplatData splat, 
     float3 centerWorldPos, float3 centerViewPos, float3 centerClipPos,
     float3x3 rotScaleMat, 
